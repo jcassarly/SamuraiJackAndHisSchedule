bfeb00dc9931359c1120127e97ec6f806ac30bd9
rosebeats <sophie.d21401@gmail.com>
2019-03-08 16:31:03 -0500

    transitioned project away from create-react-app

---
+++ web/webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        contentBase: './public'
+    }
+}
