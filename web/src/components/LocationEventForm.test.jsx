766e3251212153445ced80c3a0561d31c500ecce
jcassarly <jaredcassarly@case.edu>
2019-03-18 18:13:38 -0400

    set location event to check for nonexistence of lock event

--- web/src/components/LocationEventForm.test.jsx
+++ web/src/components/LocationEventForm.test.jsx
@@ -12,18 +12,9 @@ const store = createStore(rootReducer);
 afterEach(cleanup);
 
 test('changes lock value correctly', () => {
-    const { getByPlaceholderText } = render(
+    const { queryByPlaceholderText } = render(
         <Provider store={store}><LocationEventForm returnHome={() => {}} /></Provider>,
     );
 
-    const input = getByPlaceholderText('Lock Event');
-    expect(input.value).toEqual(''); // starts as empty string
-
-    fireEvent.click(input);
-
-    expect(input.value).toBeFalsy();
-
-    fireEvent.click(input);
-
-    expect(input.value).toEqual('');
+    expect(queryByPlaceholderText('Lock Event')).toBeNull();
 });
