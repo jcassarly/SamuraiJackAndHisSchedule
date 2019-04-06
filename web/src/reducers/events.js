c4f4dc7c5d6a9cdf6bc4bb091489be840719a2ee
rosebeats <sophie.d21401@gmail.com>
2019-04-06 18:46:06 -0400

    added comments to the action creators and reducers

--- web/src/reducers/events.js
+++ web/src/reducers/events.js
@@ -2,23 +2,38 @@ import moment from 'moment';
 import { CREATE_EVENT, CREATE_DEADLINE_EVENT } from '../actions/createEvent';
 import autoSchedule from '../events/AutoScheduler';
 
+// the user starts out with no events
+// each event needs an id, these ids are assigned in order, maxId keeps track of the largest
 const initialState = { maxId: 0, events: {} };
 
+/**
+ * reducer for the list of all events the user has
+ */
 const reducer = (state = initialState, action) => {
+    // copy the old state
     const newState = { ...state };
+
     if (action) {
+        // mutate state depending on the type of the action
         switch (action.type) {
+        // adds a new event
         case CREATE_EVENT:
+            // adds a new event with an id corresponding to max id
             newState.events[state.maxId] = action.payload.event;
             newState.maxId += 1;
             break;
+
+        // adds a new deadline event
         case CREATE_DEADLINE_EVENT: {
+            // the autoscheduler generates a new list of events based on the deadline
             const newEvents = autoSchedule(
                 state.events,
                 action.payload.deadline,
                 moment().hour(9),
                 moment().hour(17),
             );
+
+            // the new list of events is are put on the calendar, overwriting the old ones
             newState.events = {
                 ...newEvents,
             };
--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import events from './events';
 
+/**
+ * primary reducer, combines all reducers
+ */
 const rootReducer = combineReducers({
     events,
 });
