c82c5dd4a54446513d8214e3c6ae35e27f902ac9
rosebeats <sophie.d21401@gmail.com>
2019-04-21 01:15:39 -0400

    added README.md to share directory
    added README in order to help with updating of shared code.  Also
    changed set_share scripts to be more intuitive.

--- native/set_share.sh
+++ native/set_share.sh
@@ -1,15 +1,18 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-printf "${RED}WARNING${NC}: running this script will remove all share files in the ${RED}SHARE${NC} directory!\n"
+printf "${RED}WARNING${NC}: running this script will remove all share files in the ${RED}SHARE${NC} and ${RED}WEB${NC} directories!\n"
 echo "deleted directory will be:"
-printf "$GREEN$(readlink -f ../share)${NC}\n\n"
+printf "$GREEN$(readlink -f ../share)${NC}\n"
+printf "$GREEN$(readlink -f ../web/src/share)${NC}\n\n"
 read -p "Are you sure you want to do this (y/N)? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     cp ../share/sync_share.sh src/share
     rm -r ../share 2> /dev/null
+    rm -r ../web/src/share 2> /dev/null
     cp -r src/share ../share
     rm src/share/sync_share.sh
+    cp -r src/share ../web/src/share
 fi
